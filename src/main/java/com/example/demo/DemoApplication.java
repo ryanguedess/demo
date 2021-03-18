100be95558057c315ed1ec08b95f8de11282c2a6
Ryan <ryanguedesnaoto@gmail.com>
2021-03-18 00:26:53 -0300

    Branch Ryan

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Branch Ryan
+
 @SpringBootApplication
 public class DemoApplication {
-	//Branch RYAN
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
