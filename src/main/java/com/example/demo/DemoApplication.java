688b06a5acd8e93b85f1195b3884153f36c21a6f
Ryan <ryanguedesnaoto@gmail.com>
2021-03-18 00:40:57 -0300

    Retornei a versão

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Branch Ryan
-
 @SpringBootApplication
 public class DemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
